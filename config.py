2760f040a813b9ebca54cdfbd6ebb5b47402c183
unknown <xaxk047@CND910024C.us.grainger.com>
2015-04-30 13:43:08 -0500

    working for 3 it seems

--- config.py
+++ config.py
@@ -20,6 +20,7 @@ FAILED = 2
 YIELD = 3
 INQUIRE = 4
 RELEASE = 5
+TOLATE = 6
 
 NUM_IDENTIFIER_BITS = 8
 MAX_NODES = 2**NUM_IDENTIFIER_BITS
@@ -36,33 +37,41 @@ log_messages = False
 ####################################
 
 # Number of fixed Nodes
-N = 9
+N = 3
+
+# Number of nodes in voting set
+M = 2
 
 # Time a processor spends in the critical section (common for all threads) in milliseconds
-cs_int = 1
+cs_int = 0
 
 # Time a processor stays in the (release state) in milliseconds
-next_req = 1
+next_req = 0
 
 # Total time to run hte program in seconds
-tot_exec_time = 5
+tot_exec_time =  0
 
 # size of voting set
-k = 5
+k = 2
+
+# print option on received msg
+option = 0
 
 # 0 1 2
 # 3 4 5
 # 6 7 8
 # Voting Sets
-V0 = [0,1,2,3,6]
-V1 = [0,1,2,4,7]
-V2 = [0,1,2,5,8]
-V3 = [3,4,5,0,6]
-V4 = [3,4,5,1,7]
-V5 = [3,4,5,2,8]
-V6 = [6,7,8,0,3]
-V7 = [6,7,8,1,6]
-V8 = [6,7,8,2,5]
+V0 = [0,1]
+V1 = [1,2]
+V2 = [2,0]
+
+#V2 = [0,1,5,8]
+V3 = [4,5,0,6]
+V4 = [3,5,1,7]
+V5 = [3,4,2,8]
+V6 = [7,8,0,3]
+V7 = [6,8,1,6]
+V8 = [6,7,2,5]
 
 # Critical section is occupied or not
 CS = 0
--- node.py
+++ node.py
@@ -1,143 +1,176 @@
 # Comments types
 # @checklater
 from config import *
+from msglayer import *
+from SendingLayer import *
 
+
+
+######################################
+# MAEKAWA NODE
+######################################
 class Maekawa_node(threading.Thread):
     """
         Implementing Maekawa mutal exclusion algorithm
 
     """
 
-    def __init__(self, All_msg_qs, my_q, node_id):
+    def __init__(self, begin, All_msg_qs, my_q, node_id, message_lock):
         # Node params/variables
         super(Maekawa_node, self).__init__()
         self._stoprequest = threading.Event()
         self.node_id = node_id
-        self.all_message_queues = All_msg_qs        # Each element is tuple - (dest, msg)
+        self.V = []
+        self.all_message_queues = All_msg_qs      
         self.received_message_queue = my_q
-        self.message_lock = threading.Lock()        # Lock object to enforce synchronisation on send and receive message queues
+        self.send_message_queue = list()
+        self.message_lock = message_lock     
+        self.num_recv_msg = list()    
+        self.priority_q = list()    
 
         #initialization
-        self.State = RELASED
-        self.Voted = False
-        self.Lamport_ts  = 0
+        self.State = list()
+        self.State.append("RELEASED")
+        self.Lamport_ts = list()
 
         # Initialising the messaging layer for this node
-        self.msg_layer = MessagingLayer(self.node_id, self.send_message_queue, self.all_message_queues, self.message_lock)
+        self.msg_layer = MessagingLayer(self.node_id, self.received_message_queue, self.send_message_queue, self.message_lock, self.num_recv_msg, self.State, self.Lamport_ts, self.priority_q)    
         self.msg_layer.start()
+       
+        self.send_layer = SendingLayer(self.node_id, self.all_message_queues, self.send_message_queue, self.message_lock, self.Lamport_ts)
+        self.send_layer.start()
+        
+        self.total_recv_msg = 0
+        self.begin = begin
 
+        self.initiated = 0
+        self.start_time = time.time()
         # begin algorithm
-        self.Maekawa_begin()
+        #self.maekawa_begin()
+        self.Request_num = 0
+
+        #print "node: " + str(self.node_id) + " created \n" 
 
 
     def maekawa_begin(self):
         while 1:
-           # request to enter critical section  
+           if ((time.time() - self.start_time)  > tot_exec_time):
+               sys.exit(0)
+
+           self.init_v()
+
+           #request to enter critical section  
+
+           print str(self.node_id) + " REQUESTING CRITICAL SECTION \n"
+
            self.m_request()
 
            # inside critical section
            time.sleep(cs_int/1000)
-
+           print str(self.node_id) + " INSIDE CRITICAL SECTION \n"
+           print "TIME:" + str(time.time()) + " NODE:" + str(self.node_id) + " NODE-LIST:" + str(self.V) + "\n"
            # release critical section
-           self.m_resease()
+           self.m_release()
 
            # wait until next request
            time.sleep(next_req/1000)
 
     # Perform (enter) part of maekaw algo
     def m_request(self):
-        self.State = WANTED
-        self.Lamport_ts += 1
-        self.send(REQUEST)
-        while num_recv_msg != N:
-            pass
+        self.State.append("WANTED")
+        self.send("REQUEST")
+        while self.total_recv_msg != M:
+            #print "NODE:" + str(self.node_id) +  " " + str(self.total_recv_msg) + "\n"
+            with self.message_lock:
+                #print "NODE:" + str(self.node_id) + " YEHAW:" + str(self.num_recv_msg) + "\n"
+                #print self.total_recv_msg
+                if self.num_recv_msg:
+                    value = self.num_recv_msg.pop() 
+                    self.total_recv_msg = self.total_recv_msg + value
+        #Critical section obtained 
         CS = 1
-        self.print_log()
+        self.State.append("HELD")
+        #print str(time.time()) + " Node:" + self.node_id + " VotingSet: " str(self.V) 
 
     def m_release(self):
-        self.State = RELEASED
-        self.send(RELEASE)
-
+        self.total_recv_msg = 0
+        self.State.append("RELEASED")
+        self.send("RELEASE")
 
     def join(self, timeout=None):
         self._stoprequest.set()
         super(Maekawa_node, self).join(timeout)
 
-    def send(self, dest_id, msg):
+    def send(self, msg):
         #ask all nodes for access to critical section
         with self.message_lock:
-            for i in range(N):
-                self.all_message_queues[i].append((self.Lamport_ts, msg, self.node_id ))
+            self.Request_num = self.Request_num + 1
+            self.Lamport_ts.append(1)
+            for v in self.V  :
+                self.send_message_queue.append((v, msg, self.Request_num)) 
 
+    def init_v(self):
+        if self.node_id == 0:
+            self.V = V0
+        elif self.node_id == 1:
+            self.V = V1
+        elif self.node_id == 2:
+            self.V = V2
+        elif self.node_id == 3:
+            self.V = V3
+        elif self.node_id == 4:
+            self.V = V4
+        elif self.node_id == 5:
+            self.V = V5
+        elif self.node_id == 6:
+            self.V = V6
+        elif self.node_id == 7:
+            self.V = V7
+        elif self.node_id == 8:
+            self.V = V8
+        else:
+            print "wrong"
 
     def run(self):
         while not self._stoprequest.isSet():
-            msg_tuple = None
-
-            with self.message_lock:
-                if self.received_message_queue:
-                    msg_tuple = self.received_message_queue.pop(0)
-            if msg_tuple:
-                (sender, msg) = msg_tuple
-                if isinstance(msg, str):
-                    msg_list = msg.split(' ')
-                    cmd = msg_list[0]
+            if self.begin:
+                if self.initiated == 0:
+                    self.maekawa_begin() 
+                    self.initiated = 1
 
 
-                    if log_messages:
-                        print("Run: " + msg + " from " + str(sender))
-                    # Different commands/requests handled here
-                    if cmd == "update_finger_table":
-                        self.update_finger_table(int(msg_list[1]), int(msg_list[2]))
+##############################
 
-                    elif cmd == "update_finger_table_leave":
-                        self.update_finger_table_leave(int(msg_list[1]), int(msg_list[2]), int(msg_list[3]))
-
-
-
-class Coordinator(object):
-    """
-    The coordinator for the interfacing with the Maekawa network. 
-    """
-
-    def __init__(self, node_id):
-        # Node params/variables
-        self.node_id = node_id
-        self.message_lock = threading.Lock()        # Lock object to enforce synchronisation on send and receive message queues
-
-        All_msg_qs = list()
-        for i in range(N):
-           All_msg_qs[i].append([]) 
-
-        nodes = [ Maekawa_node( all_qs = All_msg_qs, my_q = all_msg_qs[i], node_id = i) for i in range(N)
-
-        for Node in Nodes
-            Node.Start() 
-
-        # total execution time
-        sleep(tot_exec_time)
-
-        for Node in nodes
-            Node.join()
-
-        # References maintained only to stop the thread. All the commands are send through network as messages
-
-
-
-coord = None
 
 if __name__ == "__main__":
-    print sys.argv
-    coord = Coordinator(coordinator_id)
+    #print sys.argv
 
-    cs_int = sys.argv[1]
-    next_req = sys.argv[2]
-    tot_exec_time = sys.argv[3]
-    option = sys.argv[4]
+    cs_int = int(sys.argv[1])
+    next_req = int(sys.argv[2])
+    tot_exec_time = float(sys.argv[3])
+    option = int(sys.argv[4])
 
-else:
-    # Start a coordinator anyway
-    coord = Coordinator(coordinator_id)
+    begin = list()
+
+    All_msg_qs = list()
+    for i in range(N):
+        # you may have a problem here
+        All_msg_qs.append([]) 
+
+    message_lock = threading.Lock()
+    nodes = [ Maekawa_node( begin = begin, All_msg_qs = All_msg_qs, my_q = All_msg_qs[i], node_id = i, message_lock = message_lock) for i in range(N) ]
+
+    for thread in nodes:
+        print thread 
+        thread.start() 
+   
+
+    begin.append(0)
+
+    for thread in nodes:
+        thread.join()
+
+
 
 
 
